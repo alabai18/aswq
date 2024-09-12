85ac8fb1e0322d5737c2b0229544604fcd8852c5
Трицераптор <nik.menshatov5@mail.ru>
2024-09-12 11:24:24 +0400

    Add files via upload

---
+++ ALABAI20/ALABAI20/Form1.cs
@@ -0,0 +1,32 @@
+using System.Windows.Forms;
+
+namespace ALABAI20
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string imagePath = @"\\192.168.3.250\Veda\3 курс\ИСП 33-9, ИСП 34-11\МДК 03.02\меньшатов шаранов\ежик.jpg";
+
+            if (File.Exists(imagePath))
+            {
+                pictureBox1.Image = Image.FromFile(imagePath);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string imagePath = @"\\192.168.3.250\Veda\3 курс\ИСП 33-9, ИСП 34-11\МДК 03.02\меньшатов шаранов\КОТ.jpg";
+
+            if (File.Exists(imagePath))
+            {
+                pictureBox2.Image = Image.FromFile(imagePath);
+            }
+        }
+    }   
+}
